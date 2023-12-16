8a74badb69aab5539bdd6a10ff30365e3c8f8ac6
Prashant <751769@student.dusd.net>
2023-12-16 12:58:30 -0800

    Done
    Finished with the assignment. Just have to do GDD and record.

--- Ruby'sAdventureChalaune/Assets/Scripts/Image.cs
+++ Ruby'sAdventureChalaune/Assets/Scripts/Image.cs
@@ -1,3 +1,4 @@
 ﻿public class Image
 {
+    internal object rectTransform;
 }
--- Ruby'sAdventureChalaune/Assets/Scripts/UIHealthBar.cs
+++ Ruby'sAdventureChalaune/Assets/Scripts/UIHealthBar.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIHealthBar : MonoBehaviour
 {
-    public static UIHealthBar instance {  get; private set; }
+    public static UIHealthBar instance { get; private set; }
 
     public Image mask;
     float originalSize;
@@ -21,8 +22,6 @@ public class UIHealthBar : MonoBehaviour
 
     public void SetValue(float value)
     {
-        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize *  value);
+        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * value);
     }
-    
-
 }
